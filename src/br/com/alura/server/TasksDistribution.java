583b11b3acfb0b793c6e176036ee715fd7b268f1
Alexandre Marinho de Souza Júnior <alexandre.masj@gmail.com>
2021-07-27 09:45:01 -0300

    Modification on TasksDistribution Class
    Made some modification to simulate a better communication with client
    and server

--- src/br/com/alura/server/TasksDistribution.java
+++ src/br/com/alura/server/TasksDistribution.java
@@ -1,6 +1,7 @@
 package br.com.alura.server;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -21,11 +22,29 @@ public class TasksDistribution implements Runnable{
 		try {
 			Scanner clientInput = new Scanner(socket.getInputStream());
 			
+			PrintStream output = new PrintStream(socket.getOutputStream());
+			
 			while(clientInput.hasNextLine()) {
 				String command = clientInput.nextLine();
+				
+				System.out.println("Executando comando " + command);
+				switch (command) {
+				case "c1": {
+					output.println("Confirmação comando C1");
+					break;
+				}
+				case "c2": {
+					output.println("Confirmação comando C2");
+					break;
+				}
+				default:
+					output.println("Comando não encontrado");
+				}
+				
 				System.out.println(command);
 			}
 			
+			output.close();
 			clientInput.close();
 		} catch (IOException e) {
 			e.printStackTrace();
