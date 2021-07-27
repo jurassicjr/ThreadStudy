b53c8f577095b38689a8304e757e1b2396aad9b4
Alexandre Marinho de Souza Júnior <alexandre.masj@gmail.com>
2021-07-27 09:27:23 -0300

    Changes in TasksClient Class
    Now the client can send and receive data at the same time, when the
    client send a "nothing" information the program exits!

--- src/br/com/alura/client/TasksClient.java
+++ src/br/com/alura/client/TasksClient.java
@@ -8,21 +8,59 @@ import java.util.Scanner;
 
 public class TasksClient {
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+
 		Socket socket = new Socket("localhost", 11011);
-		
+
 		System.out.println("---- connection established ----");
+
+		Thread sendCommandThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try (PrintStream output = new PrintStream(socket.getOutputStream())) {
+
+					try (Scanner keyboard = new Scanner(System.in)) {
+
+						System.out.println("--- Pronto para enviar dados ---");
+
+						while (keyboard.hasNextLine()) {
+							String line = keyboard.nextLine();
+
+							if (line.trim().equals("")) {
+								break;
+							}
+
+							output.println(line);
+						}
+					}
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
+
+		Thread receiveCommandThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try (Scanner serverInput = new Scanner(socket.getInputStream())) {
+					System.out.println("--- Pronto para receber dados ---");
+					while (serverInput.hasNext()) {
+						String line = serverInput.nextLine();
+						System.out.println(line);
+					}
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
+
+		sendCommandThread.start();
+		receiveCommandThread.start();
 		
-		PrintStream output = new PrintStream(socket.getOutputStream());
-		output.println("c1");
+		sendCommandThread.join();
 		
-		Scanner keyboard = new Scanner(System.in);
-		
-		keyboard.nextLine();
-		
-		output.close();
-		keyboard.close();
 		socket.close();
 	}
 
